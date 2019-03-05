53d1d5981d13bbed1481de94eabc84c5e15ea315
Eric Wittenberg <ebwittenberg@gmail.com>
2019-03-05 14:36:08 -0500

    adds functions up to most titles

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,34 @@ function charactersStartWithA(arr) {
         // include person if their name starts with a
         return person.name[0] === 'A';
     })
-    return aCharacters;
+    return aCharacters.length;
 }
 
 // How many characters start with "Z"?
 function charactersStartWithZ(arr) {
     let zCharacters = arr.filter(function(person) {
-        // include person if their name starts with a
+        // include person if their name starts with z
         return person.name[0] === 'Z';
     })
-    return zCharacters;
+    return zCharacters.length;
 }
+
+// How many characters are dead?
+function deadCharacters(arr) {
+    let deadCharacters = arr.filter(function(person) {
+        // include person if dead
+        return person.died;
+    })
+    return deadCharacters.length;
+}
+
+// Who has the most titles?
+function mostTitles(arr) {
+    let mostTitles = arr.sort(function(characterA, characterB) {
+        return characterB.titles.length - characterA.titles.length;
+    })
+    return mostTitles[0].name;
+}
+
+
+
