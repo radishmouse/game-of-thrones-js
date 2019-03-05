daa83423c196714d9f134bae9d04a32931f3a652
Matt Raines <mraines4@gmail.com>
2019-03-05 15:10:33 -0500

    adds dead chars

--- scripts/index.js
+++ scripts/index.js
@@ -6,21 +6,33 @@ function namesWithA(chars) {
     const aNames = chars.filter(function (each) {
         return each.name[0] === 'A';
     });
-    const nameOnly = aNames.map(function (each) {
-        return each.name
-    });
-
-    return nameOnly;
+    // const nameOnly = aNames.map(function (each) {
+    //     return each.name
+    // });
+    return aNames.length;
 }
 console.log(namesWithA(characters))
 
 // How many characters names start with "Z"?
 
-
+function namesWithZ(chars) {
+    const zNames = chars.filter(function (each) {
+        return each.name[0] === 'Z';
+    });
+    return zNames.length;
+}
+console.log(namesWithZ(characters))
 
 // How many characters are dead?
 
+function deadCharacters(chars) {
+    const dead = chars.filter(function (each) {
+        return each.died;
+    });
+    return dead.length;
+}
 
+console.log(deadCharacters(characters));
 
 // Who has the most titles?
 
