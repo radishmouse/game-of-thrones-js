a5b87e85a5d5d8cf9d38700dee76e5ccb7b47e77
Eric Wittenberg <ebwittenberg@gmail.com>
2019-03-05 14:22:09 -0500

    adds characters start with functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
 console.log(`There are ${characters.length} characters in the array.`);
+
+// How many characters names start with "A"?
+function charactersStartWithA(arr) {
+    let aCharacters = arr.filter(function(person) {
+        // include person if their name starts with a
+        return person.name[0] === 'A';
+    })
+    return aCharacters;
+}
+
+// How many characters start with "Z"?
+function charactersStartWithZ(arr) {
+    let zCharacters = arr.filter(function(person) {
+        // include person if their name starts with a
+        return person.name[0] === 'Z';
+    })
+    return zCharacters;
+}
