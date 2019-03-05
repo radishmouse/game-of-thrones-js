56ce7fa4cdddd40dda2096b9c98d0d3683a98831
Margaret O'Neill <margaret@oneillfish.com>
2019-03-05 15:06:16 -0500

    wrote BeginsWithLetter function

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 console.log(`There are ${characters.length} characters in the array.`);
+
+// console.log(Object.keys(characters));
+//split characters into arrays
+
+function beginsWithLetter(char,letter){
+    let count = 0;
+    char.forEach(function(person){
+        if (person.name[0] === letter){
+            count += 1;
+            // console.log(person.name)
+        }
+    })
+    return count;
+}
+console.log(`There are ${beginsWithLetter(characters,"A")} characters with names beginning with A`);
+console.log(`There are ${beginsWithLetter(characters,"Z")} characters with names beginning with Z`);
