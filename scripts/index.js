0999a94a65f050c774ac60c32ad12f3267ea6624
Matt Raines <mraines4@gmail.com>
2019-03-05 14:59:30 -0500

    adds all the excercise  comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,37 @@
 console.log(`There are ${characters.length} characters in the array.`);
+
+// How many characters names start with "A"?
+
+
+
+// How many characters names start with "Z"?
+
+
+
+// How many characters are dead?
+
+
+
+// Who has the most titles?
+
+
+
+// How many are Valyrian?
+
+
+
+// What actor plays "Hot Pie" (and don't use IMDB)?
+
+
+
+// How many characters are *not* in the tv show?
+
+
+
+// Produce a list characters with the last name "Targaryen"
+
+
+
+// Create a histogram of the houses (it's the "allegiances" key)
+
+
