ed959a24b3b5a0c2cbb7a79115a0ed8aa005c442
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-06 08:21:39 -0500

    added how many are dead challenge

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,26 @@
 console.log(`There are ${characters.length} characters in the array.`);
 
 function howManyStartWith(letter){
-    results = 0
+    results = 0;
     characters.forEach(function (charObj){
         if (charObj.name[0] === letter){
-            results +=1
+            results +=1;
         }
     })
-    return results
+    return results;
 }
 
 // console.log(howManyStartWith("A"))
 // console.log(howManyStartWith("Z"))
+
+function howManyDead(){
+    results = 0;
+    characters.forEach(function(charObj){
+        if (charObj.died.length > 0){
+            results += 1;
+        }
+    })
+    return results
+}
+// console.log(howManyDead())
+
