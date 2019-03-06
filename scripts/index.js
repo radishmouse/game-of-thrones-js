3821a1298198f23e98980add8a08bf216f363e57
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-06 08:24:40 -0500

    corrected syntax

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 console.log(`There are ${characters.length} characters in the array.`);
 
 function howManyStartWith(letter){
-    results = 0;
+    let results = 0;
     characters.forEach(function (charObj){
         if (charObj.name[0] === letter){
             results +=1;
@@ -14,13 +14,13 @@ function howManyStartWith(letter){
 // console.log(howManyStartWith("Z"))
 
 function howManyDead(){
-    results = 0;
+    let results = 0;
     characters.forEach(function(charObj){
         if (charObj.died.length > 0){
             results += 1;
         }
     })
-    return results
+    return results;
 }
 // console.log(howManyDead())
 
