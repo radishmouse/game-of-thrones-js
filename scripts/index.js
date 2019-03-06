e44caab41144ab9cbbc2d8b6a3001d7e98674a04
Eric Mauldin <emauldin84@gmail.com>
2019-03-05 22:27:11 -0500

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,42 @@
-console.log(`There are ${characters.length} characters in the array.`);
+// console.log(`There are ${characters.length} characters in the array.`);
+
+console.log (characters[0].died)
+
+// How many character names start with A
+// function beginsWithA(character) {
+//     return character.name.startsWith('A');
+// }
+
+// function findsACharacters(book) {
+//     aCharacters = book.filter(beginsWithA)
+//     return aCharacters.length
+// }
+// console.log(findsACharacters(characters))
+
+
+// How many character names start with Z
+// function beginsWithZ(character) {
+//     return character.name.startsWith('Z');
+// }
+
+// function findsACharacters(book) {
+//     aCharacters = book.filter(beginsWithZ)
+//     return aCharacters.length
+// }
+// console.log(findsACharacters(characters))
+
+// //How many characters are dead?
+deadCharacters = characters.forEach(function(key) {
+    deadList = [];
+    for (let i = 0; i < characters[key].length; i++) {
+        if (key.died.length > 0) {
+        deadList.push(characters.died);
+        }
+        
+    }
+    // console.log(key.died.length)
+    return deadList;
+})
+console.log(deadCharacters)
+
+
