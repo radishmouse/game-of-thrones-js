e3875ebf02f86b095fe94f374c11c27adbf5ca39
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-06 08:16:25 -0500

    added how many names challenge

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 console.log(`There are ${characters.length} characters in the array.`);
+
+function howManyStartWith(letter){
+    results = 0
+    characters.forEach(function (charObj){
+        if (charObj.name[0] === letter){
+            results +=1
+        }
+    })
+    return results
+}
+
+// console.log(howManyStartWith("A"))
+// console.log(howManyStartWith("Z"))
