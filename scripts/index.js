50dade94744baa8dd88d3d2e0741f3baf49b9f46
Eric Mauldin <emauldin84@gmail.com>
2019-03-06 09:05:08 -0500

    adds everything up to the house histogram exercise

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // console.log(`There are ${characters.length} characters in the array.`);
 
-console.log (characters[0].died)
+// console.log (characters[0].died)
 
 // How many character names start with A
 // function beginsWithA(character) {
@@ -14,7 +14,7 @@ console.log (characters[0].died)
 // console.log(findsACharacters(characters))
 
 
-// How many character names start with Z
+// // How many character names start with Z
 // function beginsWithZ(character) {
 //     return character.name.startsWith('Z');
 // }
@@ -26,17 +26,65 @@ console.log (characters[0].died)
 // console.log(findsACharacters(characters))
 
 // //How many characters are dead?
-deadCharacters = characters.forEach(function(key) {
-    deadList = [];
-    for (let i = 0; i < characters[key].length; i++) {
-        if (key.died.length > 0) {
-        deadList.push(characters.died);
-        }
-        
-    }
-    // console.log(key.died.length)
-    return deadList;
-})
-console.log(deadCharacters)
+function deadCharacters (characterList) {
+    let deadArray = characterList.filter(function(personName) {
+        return personName.died !== '';
+    })
+    return deadArray.length;
+}
+console.log(deadCharacters(characters));
+
+// Who has the most titles?
+function mostTitles(characterList) {
+    characterList.sort(function(a,b) {
+        return (b.titles.length - a.titles.length)
+    })
+    return characterList[0].name;
+}
+console.log(mostTitles(characters));
+
+// How many are Valyrian?
+function areVarlyrian(characterList) {
+    let valyrianArray = characterList.filter(function(personName) {
+        return personName.culture === 'Valyrian';
+    })
+    return valyrianArray.length;
+}
+console.log(areVarlyrian(characters));
+
+//What actor plays "Hot Pie" (and don't use IMDB)?
+function isHotPie(characterList) {
+    let hotPie = characterList.filter(function(personName) {
+        return personName.name === "Hot Pie";
+    })
+    return hotPie[0].playedBy;
+}
+console.log(isHotPie(characters));
+
+// How many characters are *not* in the tv show?
+function notInShow(characterList) {
+    let notInShowArray = characterList.filter(function(personName){
+        return personName.tvSeries == '';
+    })
+    return notInShowArray.length
+}
+console.log(notInShow(characters));
 
 
+// Produce a list characters with the last name "Targaryen"
+
+function isTargaryen(characterList) {
+    let targaryenArray = characterList.filter(function(personName){
+        return personName.name.includes('Targaryen');
+    });
+    targaryenNamesArray = targaryenArray.map(function(personName){
+        return personName.name;
+    })
+    return targaryenNamesArray;
+}
+console.log(isTargaryen(characters));
+
+// Create a histogram of the houses (it's the "allegiances" key)
+function houseHistogram(characterList) {
+    
+}
